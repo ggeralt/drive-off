8b0517de928557b54ed75d9539078513c391e19c
ggeralt <jan.santek.455@gmail.com>
2023-05-22 19:01:56 +0200

    Add new WPF project: RentACarDesktop

---
+++ RentACarDesktop/Login.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace RentACarDesktop
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
